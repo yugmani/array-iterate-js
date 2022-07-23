b2f62e2bf2446e4b1da41834cac0852e1942ab93
yugmani <yugmani@yahoo.com>
2022-07-23 12:17:16 -0700

    using do ... while loo

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // Import stylesheets
 import './style.css';
 
+// *************************************************
+// Looping Through an Array in JavaScript
+// *************************************************
+
 //create an array
 const arr1 = ['Mayan', 23, 'Los Angeles', 'Software engineer'];
-// Using while {...} loop
+
+// Using `while {...}` loop
+// ***********************
+
 let i = 0;
 while (i < arr1.length) {
   let item = arr1[i];
   console.log(item);
   i++;
 }
+
+console.log(i);
+//Mayan 23 Los Angeles Software engineer
+
+// Using `do {...} while` loop
+let k = 0;
+let myString = '';
+
+do {
+  let item = arr1[k];
+  myString += item;
+  ++k;
+} while (k < arr1.length);
+
+console.log(myString);
