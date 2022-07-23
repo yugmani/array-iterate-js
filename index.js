afe50b339c961e0a7a5c210bf0620a1b7ad1f96c
yugmani <yugmani@yahoo.com>
2022-07-23 12:08:41 -0700

    looping through an array using while loop.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Import stylesheets
 import './style.css';
 
+//create an array
+const arr1 = ['Mayan', 23, 'Los Angeles', 'Software engineer'];
+// Using while {...} loop
+let i = 0;
+while (i < arr1.length) {
+  let item = arr1[i];
+  console.log(item);
+  i++;
+}
